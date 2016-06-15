5e65b610a032e8de6a4d83db7189877eee6eb09e
Martin Cross <mncross@gmail.com>
2016-06-15 11:10:45 -0500

    setup Entity Core, build Models, and build DB

---
+++ src/CandH_API/Models/ComicStrip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CandH_API.Models
+{
+  public class ComicStrip
+  {
+    public int ComicStripId { get; set; }
+    public string Name { get; set; }
+    public DateTime? OriginalPrintDate { get; set; }
+    public string Transcript { get; set; }
+    public byte[] Image { get; set; }
+
+    public ICollection<ComicStripEmotion> Emotions { get; set; }
+    public ICollection<FavoriteComicStrip> Favorited { get; set; }
+    public ICollection<ComicStripComment> Comments { get; set; }
+  }
+}
---
+++ src/CandH_API/Models/ComicStripComment.cs
@@ -0,0 +1,13 @@
+﻿namespace CandH_API.Models
+{
+  public class ComicStripComment
+  {
+    public int ComicStripCommentId { get; set; }
+    public int ComicStripId { get; set; }
+    public int ComicUserId { get; set; }
+    public string Comment { get; set; }
+
+    public ComicStrip Strip { get; set; }
+    public ComicUser Reader { get; set; }
+  }
+}
