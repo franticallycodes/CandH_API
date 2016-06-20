fa09f5abcf15be6a1b31ad061c2996ca75e2ea0a
Martin Cross <mncross@gmail.com>
2016-06-20 14:21:04 -0500

    build basic GET for distinct emotions

---
+++ src/CandH_API/Controllers/EmotionController.cs
@@ -0,0 +1,41 @@
+﻿using CandH_API.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandH_API.Controllers
+{
+  [Route("api/[controller]")]
+  [Produces("application/json")]
+  [EnableCors("AllowDevEnvironment")]
+  public class EmotionController : Controller
+  {
+    private CandH_Context _context;
+
+    public EmotionController(CandH_Context context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GET()
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      IQueryable<ComicStripEmotion> distinctEmotions = _context.Emotion.Distinct();
+
+      if (distinctEmotions == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(distinctEmotions);
+    }
+  }
+}
